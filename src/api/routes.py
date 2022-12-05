79b37696bf1c0e1c211173e76a16b7bd98d5ec7b
ADOLFO BALSEIRO <abalseiro@hotmail.com>
2022-12-05 18:47:03 +0000

    Ejemplo JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +18,23 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.json
+
+    user = User.query.filter_by(email=data['email'], password=data['password']).first()
+
+    #user = User.query.filter_by(email=data.get['email', 'no existe el email'], password=data.get['password', 'contraseña incorrecta']).first()
+    if user:
+        token = create_access_token(identify=user.id)
+        return jsonify({'token': token}), 200
+
+    return jsonify({"message": "Usuario / contraseña incorrectos"}), 400
+
+@api.route('/user', methods=['GET'])
+@jwt_required()
+def get_user():
+    user_id = get_jwt_identity()
+    return jsonify("Esta info es muy importante y delicada")
